60883f103ecf91f3cf726f15ef9260fed7a0b88d
PabloNM <pabloneiro58@gmail.com>
2024-02-01 18:28:57 +0100

    fixed build warnings

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,7 +3,11 @@ import "./navbar.css";
 export default function Navbar() {
   return (
     <section id="navbar">
-      <img className="navbar__img" src="./logo-cochescom.png" />
+      <img
+        className="navbar__img"
+        src="./logo-cochescom.png"
+        alt="Logo Coches.com"
+      />
     </section>
   );
 }
